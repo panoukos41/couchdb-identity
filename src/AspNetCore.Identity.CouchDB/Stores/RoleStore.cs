2d832fafe9911603ed65e6c515e372ba4b220df5
Panos Athanasiou <panos_ath@outlook.com>
2021-02-22 02:07:23 +0200

    Add RoleStore class

---
+++ src/AspNetCore.Identity.CouchDB/Stores/RoleStore.cs
@@ -0,0 +1,210 @@
+﻿using AspNetCore.Identity.CouchDB.Models;
+using AspNetCore.Identity.CouchDB.Stores.Internal;
+using CouchDB.Driver;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Identity.CouchDB.Stores
+{
+    /// <inheritdoc/>
+    public class RoleStore : RoleStore<CouchDbRole>
+    {
+        public RoleStore(IServiceProvider provider, IOptionsMonitor<CouchDbIdentityOptions> options)
+            : base(provider, options)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Creates a new instance of a persistence store for roles.
+    /// </summary>
+    /// <typeparam name="TRole">The type of the class representing a role.</typeparam>
+    [SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "Nothing to dispose of.")]
+    public class RoleStore<TRole> :
+        BaseStore<TRole>,
+        IQueryableRoleStore<TRole>,
+        IRoleStore<TRole>
+        //IRoleClaimStore<TRole>
+        where TRole : CouchDbRole
+    {
+        public RoleStore(
+            IServiceProvider provider,
+            IOptionsMonitor<CouchDbIdentityOptions> options)
+            : base(provider, options)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override string Discriminator => Options.CurrentValue.RoleDiscriminator;
+
+        /// <inheritdoc/>
+        public void Dispose() { }
+
+        #region IQueryableRoleStore
+
+        /// <inheritdoc/>
+        public virtual IQueryable<TRole> Roles => GetDatabase().AsQueryable();
+
+        #endregion
+
+        #region IRoleStore
+
+        /// <inheritdoc/>
+        public virtual async Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            Check.NotNull(role, nameof(role));
+
+            role.Discriminator = Discriminator;
+            role.Id ??= Guid.NewGuid().ToString();
+            await GetDatabase().AddAsync(role, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            return IdentityResult.Success;
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IdentityResult> UpdateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            Check.NotNull(role, nameof(role));
+
+            await GetDatabase().AddOrUpdateAsync(role, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            return IdentityResult.Success;
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<IdentityResult> DeleteAsync(TRole role, CancellationToken cancellationToken)
+        {
+            Check.NotNull(role, nameof(role));
+
+            await GetDatabase().RemoveAsync(role, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            return IdentityResult.Success;
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<TRole> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return await GetDatabase().FindAsync(roleId, cancellationToken: cancellationToken).ConfigureAwait(false);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        /// <inheritdoc/>
+        public virtual async Task<TRole> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            Check.NotNull(normalizedRoleName, nameof(normalizedRoleName));
+
+            var (design, view) = CouchDbIdentityViews.Role.NormalizedName;
+            var result = await GetDatabase().GetViewAsync<TRole, int, TRole>(design, view, cancellationToken: cancellationToken, options: new()
+            {
+                Key = normalizedRoleName,
+                Limit = 1
+            }).ConfigureAwait(false);
+#pragma warning disable CS8603 // Possible null reference return.
+            return result.Rows.FirstOrDefault()?.Doc;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        /// <inheritdoc/>
+        public virtual Task<string> GetRoleIdAsync(TRole role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(role.Id);
+        }
+
+        /// <inheritdoc/>
+        public virtual Task<string> GetRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(role.Name);
+        }
+
+        /// <inheritdoc/>
+        public virtual Task<string> GetNormalizedRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(role.NormalizedName);
+        }
+
+        /// <inheritdoc/>
+        public virtual Task SetRoleNameAsync(TRole role, string roleName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Check.NotNull(role, nameof(role));
+
+            role.Name = roleName;
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc/>
+        public virtual Task SetNormalizedRoleNameAsync(TRole role, string normalizedName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Check.NotNull(role, nameof(role));
+
+            role.NormalizedName = normalizedName;
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region IRoleClaimStore
+
+        ///// <inheritdoc/>
+        //public virtual async Task AddClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default)
+        //{
+        //    Check.NotNull(role, nameof(role));
+        //    Check.NotNull(claim, nameof(claim));
+
+        //    var currentClaim = role.Claims.FirstOrDefault(c => c.ClaimType == claim.Type && c.ClaimValue == claim.Value);
+
+        //    if (currentClaim is null)
+        //    {
+        //        role.Claims.Add(new() { ClaimType = claim.Type, ClaimValue = claim.Value });
+
+        //        await GetDatabase().AddOrUpdateAsync(role, cancellationToken: cancellationToken);
+        //    }
+        //}
+
+        ///// <inheritdoc/>
+        //public virtual async Task RemoveClaimAsync(TRole role, Claim claim, CancellationToken cancellationToken = default)
+        //{
+        //    Check.NotNull(role, nameof(role));
+        //    Check.NotNull(claim, nameof(claim));
+
+        //    var count = role.Claims.Count;
+        //    role.Claims.RemoveAll(x =>
+        //        x.ClaimType == claim.Type &&
+        //        x.ClaimValue == claim.Value);
+
+        //    if (count != role.Claims.Count)
+        //    {
+        //        await GetDatabase().AddOrUpdateAsync(role, cancellationToken: cancellationToken);
+        //    }
+        //}
+
+        ///// <inheritdoc/>
+        //public virtual async Task<IList<Claim>> GetClaimsAsync(TRole role, CancellationToken cancellationToken = default)
+        //{
+        //    Check.NotNull(role, nameof(role));
+
+        //    return (await FindByIdAsync(role.Id, cancellationToken).ConfigureAwait(false))
+        //        .Claims
+        //        .Select(e => new Claim(e.ClaimType, e.ClaimValue))
+        //        .ToList();
+        //}
+
+        #endregion
+    }
+}
