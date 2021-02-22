7d8365a77964e94a09ce7282e7bd6cdbadf79dae
Panos Athanasiou <panos_ath@outlook.com>
2021-02-22 02:06:00 +0200

    Add CouchDbRole model class

---
+++ src/AspNetCore.Identity.CouchDB/Models/CouchDbRole.cs
@@ -0,0 +1,89 @@
+﻿using AspNetCore.Identity.CouchDB.Models.Internal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCore.Identity.CouchDB.Models
+{
+    /// <summary>
+    /// A CouchDb document representing a role.
+    /// </summary>
+    /// <remarks>
+    /// ToString returns the <see cref="Name"/> property.<br/>
+    /// Equality only compares the <see cref="NormalizedName"/> property.<br/>
+    /// GetHashCode returns a hash for the <see cref="NormalizedName"/> property.
+    /// </remarks>
+    public class CouchDbRole : IdentityCouchDocument, IEquatable<CouchDbRole?>
+    {
+        /// <summary>
+        /// Initialize a new <see cref="CouchDbRole"/>.
+        /// </summary>
+        public CouchDbRole()
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new <see cref="CouchDbRole"/> with a role name.
+        /// </summary>
+        /// <param name="roleName">The role name to initialize with.</param>
+        public CouchDbRole(string roleName)
+        {
+            Name = roleName;
+            NormalizedName = roleName.ToUpperInvariant();
+        }
+
+        /// <inheritdoc/>
+        public override string Discriminator { get; set; } = "aspnetcore.role";
+
+        ///<summary>
+        /// Gets or sets the name for this role.
+        ///</summary>
+        [JsonProperty("name")]
+        public virtual string Name { get; set; } = null!;
+
+        ///<summary>
+        /// Gets or sets the normalized name for this role.
+        ///</summary>
+        [JsonProperty("normalized_name")]
+        public virtual string NormalizedName { get; set; } = null!;
+
+        /// <summary>
+        /// Returns the name of the role.
+        /// </summary>
+        /// <returns>The name of the role.</returns>
+        public override string ToString() => Name;
+
+        #region Equals
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as CouchDbRole);
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(CouchDbRole? other)
+        {
+            return other is not null &&
+                   NormalizedName == other.NormalizedName;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NormalizedName);
+        }
+
+        public static bool operator ==(CouchDbRole? left, CouchDbRole? right)
+        {
+            return EqualityComparer<CouchDbRole>.Default.Equals(left!, right!);
+        }
+
+        public static bool operator !=(CouchDbRole? left, CouchDbRole? right)
+        {
+            return !(left == right);
+        }
+
+        #endregion
+    }
+}
